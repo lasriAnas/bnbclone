a63292dbbddb7ab4875914c23299f38afc1a5f83
lasriAnas <anaslasricsgo@gmail.com>
2023-05-02 00:44:06 +0200

    api next

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -1,17 +1,19 @@
 "use client";
-
+/* import { signIn } from "next-auth/react"; */
 import axios from "axios";
 import { AiFillGithub } from "react-icons/ai";
-/* import { signIn } from "next-auth/react"; */
+
 import { FcGoogle } from "react-icons/fc";
 import { useCallback, useState } from "react";
-/* import { toast } from "react-hot-toast"; */
+import { toast } from "react-hot-toast";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 
 import useRegisterModal from "@/app/hooks/useRegisterModal";
-import { error } from "console";
+
 import Modal from "./Modal";
 import Heading from "../Heading";
+import Input from "../inputs/Input";
+import Button from "../Button";
 
 const RegisterModal = () => {
   const registerModal = useRegisterModal();
@@ -38,7 +40,7 @@ const RegisterModal = () => {
         registerModal.onClose();
       })
       .catch((error) => {
-        console.log(error);
+        toast.error("Something went wrong.");
       })
       .finally(() => {
         setIsLoading(false);
@@ -48,6 +50,61 @@ const RegisterModal = () => {
   const bodyContent = (
     <div className="flex flex-col gap-4">
       <Heading title="welcome" subtitle="create account" />
+      <Input
+        id="email"
+        label="email"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+      <Input
+        id="name"
+        label="name"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+      <Input
+        id="password"
+        label="password"
+        type="password"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+    </div>
+  );
+
+  const footerContent = (
+    <div className="flex flex-col gap-4 mt-3">
+      <hr />
+      <Button
+        outline
+        label="Continue with Google"
+        icon={FcGoogle}
+        /* onClick={() => signIn("google")} */
+      />
+      <Button
+        outline
+        label="Continue with Github"
+        icon={AiFillGithub}
+        /* onClick={() => signIn("github")} */
+      />
+      <div className=" text-neutral-500 text-center mt-4 font-light">
+        <p>
+          Already have an account?
+          <span
+            /* onClick={onToggle} */
+            className="text-neutral-800 cursor-pointer hover:underline"
+          >
+            {" "}
+            Log in
+          </span>
+        </p>
+      </div>
     </div>
   );
 
@@ -60,6 +117,7 @@ const RegisterModal = () => {
       onClose={registerModal.onClose}
       onSubmit={handleSubmit(onSubmit)}
       body={bodyContent}
+      footer={footerContent}
     />
   );
 };
